d518d804e0ef77fb2f3a5ed77d7d1fe20b369741
Shahan <shahan.ahmed@.apify.ai>
2022-10-17 10:50:49 +0500

    reactive form implementation completed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,12 +7,36 @@ import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 	styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+	name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+	email = new FormControl('', [Validators.required, Validators.email]);
+	age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]);
+	password = new FormControl('', [
+		Validators.required,
+		Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+	]);
+	confirmPassword = new FormControl('', [Validators.required]);
+	phone = new FormControl('', [
+		Validators.required,
+		Validators.minLength(12),
+		Validators.maxLength(12)
+	]);
+
 	registerForm = new FormGroup({
-		name: new FormControl('', [Validators.required]),
-		email: new FormControl(''),
-		age: new FormControl(''),
-		password: new FormControl(''),
-		confirmPassword: new FormControl(''),
-		phone: new FormControl('')
+		name: this.name,
+		email: this.email,
+		age: this.age,
+		password: this.password,
+		confirmPassword: this.confirmPassword,
+		phone: this.phone
 	});
+
+	register() {
+		this.showAlert = true;
+		this.alertMessage = 'Please wait, your account is being created';
+		this.alertColor = 'blue';
+	}
+
+	showAlert = false;
+	alertMessage = 'Please wait, your account is being created';
+	alertColor = 'blue'
 }
