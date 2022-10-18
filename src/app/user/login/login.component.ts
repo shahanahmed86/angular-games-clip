32183d536eb957f51135966233aefd9f16a779e1
Shahan <shahan.ahmed@.apify.ai>
2022-10-18 15:20:34 +0500

    form with template is done

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	showAlert = false;
+	alertMessage = 'Please wait, your account is being signing in';
+	alertColor = 'blue'
+	credentials = { email: '', password: '' };
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
+  
+	login() {
+		this.showAlert = true;
+		this.alertMessage = 'Please wait, your account is being signing in';
+    this.alertColor = 'blue';
+    
+    console.log(this.credentials)
+	}
 }
